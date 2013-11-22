7fac6a6ddb010623986525d5388096e1b74fefb8
Todd Wolfson <todd@twolfson.com>
2013-11-22 03:34:42 -0800

    Thinned out unnecessary content

--- sublimelinter/modules/restructuredtext.py
+++ sublimelinter/modules/restructuredtext.py
@@ -1,20 +1,16 @@
-import re
-
 from base_linter import BaseLinter
 
 CONFIG = {
     'language': 'reStructuredText',
-    'executable': 'ruby',
-    'lint_args': '-wc'
 }
 
-
 class Linter(BaseLinter):
+    def built_in_check(self, view, code, filename):
+        errors = []
+
+        print 'xxx'
+
+        return errors
+
     def parse_errors(self, view, errors, lines, errorUnderlines, violationUnderlines, warningUnderlines, errorMessages, violationMessages, warningMessages):
         print 'hai'
-        for line in errors.splitlines():
-            match = re.match(r'^.+:(?P<line>\d+):\s+(?P<error>.+)', line)
-
-            if match:
-                error, line = match.group('error'), match.group('line')
-                self.add_message(int(line), lines, error, errorMessages)
